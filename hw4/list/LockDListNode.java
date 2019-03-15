f85fd3451326b9b008f051dac0342e0a2f92c1a7
Mike-Dai <mrmikedai@outlook.com>
2019-03-15 19:52:40 +0800

    Finish hw4

---
+++ hw4/list/LockDListNode.java
@@ -0,0 +1,13 @@
+package list;
+
+public class LockDListNode extends DListNode {
+	
+	protected boolean isLocked;
+
+
+
+	LockDListNode(Object i, DListNode p, DListNode n) {
+		super(i, p, n);
+		isLocked = false;
+	}
+}
